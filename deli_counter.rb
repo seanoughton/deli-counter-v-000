20c5a69eb90cbd8b6f47503c6fff2397e12831c9
Sean Oughton <seanoughton@gmail.com>
2018-01-26 18:21:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@
 
 def line(array)
   new_string = ""
-  array.each_with_index do |name,index|  #iterate over the array and get name and index
-    new_string << (index+1).to_s + ". " + name + " "  #add each string at each position to a new string
-  end
-  if array.size == 0
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{new_string}"
-  end
+    array.each_with_index do |name,index|  #iterate over the array and get name and index
+      new_string << " " + (index+1).to_s + ". " + name   #add each string at each position to a new string
+    end
+    if array.size == 0
+      puts "The line is currently empty."
+    else
+      puts "The line is currently:#{new_string}"
+    end
 end
 
 def take_a_number(array,name_of_person)
